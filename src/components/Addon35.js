897f723a81e735551d6c6e14e4d891a8b41701f6
ezgiturk <eturkokuloglu@gmail.com>
2019-07-31 22:02:30 +0300

    newcommit

--- src/components/Addon35.js
+++ src/components/Addon35.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import Badge from 'react-bootstrap/Badge';
+
 
 class Addon17 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon35</div>" ></FiddleComponent>
-                Ezgi Turkokuloglu
+                <FiddleComponent code="<div>this is Addon1 <Badge pill variant='primary'>
+                    Primary
+                </Badge> </div>" scope={scope} ></FiddleComponent>
+                <br/>Ezgi Turkokuloglu
             </div>
         );
     }
